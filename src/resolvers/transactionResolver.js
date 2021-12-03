6a987f0e9cbeda7f0a2a8b866546c439a0cbb2b6
casierrav <jerarquia04@gmail.com>
2021-12-02 21:12:54 -0500

    Add issue and change name of deployment app

--- src/resolvers/transactionResolver.js
+++ src/resolvers/transactionResolver.js
@@ -1,14 +1,14 @@
 const transactionResolver = {
     Query: {
         transactionByUsername: async(_, { username }, { dataSources, userIdToken}) => {
-            usernameToken = (await dataSources.authAPI.get(userIdToken)).username
+            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
             if(username == usernameToken) 
                 return await dataSources.accountAPI.transactionsByUsername(username);
             else
                 return null;
         }, 
         transactionById: async(_, { transactionId}, { dataSources, userIdToken} ) => {
-            usernameToken       = (await dataSources.authAPI.get(userIdToken)).username
+            usernameToken       = (await dataSources.authAPI.getUser(userIdToken)).username
             const transaction   = await dataSources.accountAPI.transactionsById( transactionId )
             usernameTransaction = transaction.usernameOrigin
             if(usernameToken == usernameTransaction)
@@ -19,14 +19,14 @@ const transactionResolver = {
     },
     Mutation: {
         createTransaction: async(_, { transaction }, { dataSources, userIdToken }) => {
-            usernameToken = (await dataSources.authAPI.get(userIdToken)).username
+            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
             if(transaction.usernameOrigin == usernameToken)
                 return await dataSources.accountAPI.createTransaction(transaction);
             else
                 return null;
         },
         updateTransaction: async(_, { transaction }, { dataSources, userIdToken }) => {
-            usernameToken       = (await dataSources.authAPI.get(userIdToken)).username
+            usernameToken       = (await dataSources.authAPI.getUser(userIdToken)).username
             usernameTransaction = (await dataSources.accountAPI.transactionsById(transaction.id)).usernameOrigin
             if(usernameToken == usernameTransaction)
                 return await dataSources.accountAPI.updateTransaction(transaction);
@@ -35,7 +35,7 @@ const transactionResolver = {
 
         },
         deleteTransaction: async(_, { username }, { dataSources, userIdToken }) => {
-            usernameToken = (await dataSources.authAPI.get(userIdToken)).username
+            usernameToken = (await dataSources.authAPI.getUser(userIdToken)).username
         }
     }
 };
